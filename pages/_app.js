d9f77206467af91d7259febb061bbd297ef3be34
Ramazan <ozturkiramazan@gmail.com>
2021-12-31 23:05:51 +0300

    basic design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,55 @@
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Checkbox,
+  CheckboxGroup,
+  Stack
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
 function HomePage() {
-  return <div className="text-purple-900">Welcome to Next.js!</div>
+  const [sliderValue, setSliderValue] = useState(3);
+
+  return (
+    <div className="pt-20 antialiased">
+      <div className="mx-auto w-full max-w-sm relative mx-auto shadow-lg rounded-lg border">
+        <h1 className="text-center text-xl mb-4 pt-5">Password Generator</h1>
+        <div className="border-b-2 py-3">
+          <input className="pl-5 pr-16 w-full focus:outline-0 font-normal text-lg" />
+        </div>
+        <div className="p-5">
+          <div className="mb-1">
+            Length <span className="font-medium">{sliderValue}</span>
+          </div>
+          <Slider
+            aria-label="slider-ex-1"
+            defaultValue={sliderValue}
+            min={1}
+            max={32}
+            step={1}
+            onChange={(val) => setSliderValue(val)}
+          >
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb boxSize={5} bg="blue.500" />
+          </Slider>
+        </div>
+        <div className="p-5">
+          <CheckboxGroup>
+            <div className="flex flex-wrap justify-between">
+              <Checkbox>Uppercase</Checkbox>
+              <Checkbox>Lowercase</Checkbox>
+              <Checkbox>Numbers</Checkbox>
+              <Checkbox>Symbols</Checkbox>
+            </div>
+          </CheckboxGroup>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
